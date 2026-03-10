65abab2dbafd606bf83382117eb85a73ee88b534
sigmdel <michel@sigmdel.ca>
2026-03-10 01:46:17 -0300

    More  WIFI_POWER_xxdBm values and add dhcp times in summary

--- 05_wifi_tx_power/wifi_tx_power/main.cpp
+++ 05_wifi_tx_power/wifi_tx_power/main.cpp
@@ -47,59 +47,74 @@
  from ~/.platformio/packages/framework-arduinoespressif32/libraries/WiFi/src/WiFiGeneric.h
 
 typedef enum {
-    WIFI_POWER_19_5dBm = 78,// 19.5dBm
-    WIFI_POWER_19dBm = 76,// 19dBm
-    WIFI_POWER_18_5dBm = 74,// 18.5dBm
-    WIFI_POWER_17dBm = 68,// 17dBm
-    WIFI_POWER_15dBm = 60,// 15dBm
-    WIFI_POWER_13dBm = 52,// 13dBm
-    WIFI_POWER_11dBm = 44,// 11dBm
-    WIFI_POWER_8_5dBm = 34,// 8.5dBm
-    WIFI_POWER_7dBm = 28,// 7dBm
-    WIFI_POWER_5dBm = 20,// 5dBm
-    WIFI_POWER_2dBm = 8,// 2dBm
-    WIFI_POWER_MINUS_1dBm = -4// -1dBm
+  WIFI_POWER_21dBm = 84,      // 21dBm
+  WIFI_POWER_20_5dBm = 82,    // 20.5dBm
+  WIFI_POWER_20dBm = 80,      // 20dBm
+  WIFI_POWER_19_5dBm = 78,    // 19.5dBm
+  WIFI_POWER_19dBm = 76,      // 19dBm
+  WIFI_POWER_18_5dBm = 74,    // 18.5dBm
+  WIFI_POWER_17dBm = 68,      // 17dBm
+  WIFI_POWER_15dBm = 60,      // 15dBm
+  WIFI_POWER_13dBm = 52,      // 13dBm
+  WIFI_POWER_11dBm = 44,      // 11dBm
+  WIFI_POWER_8_5dBm = 34,     // 8.5dBm
+  WIFI_POWER_7dBm = 28,       // 7dBm
+  WIFI_POWER_5dBm = 20,       // 5dBm
+  WIFI_POWER_2dBm = 8,        // 2dBm
+  WIFI_POWER_MINUS_1dBm = -4  // -1dBm
 } wifi_power_t;
 */
 
-const int powerCount = 13;
+const int powerCount = 16;
 
 int power[powerCount] = {
-  0,
+  WIFI_POWER_MINUS_1dBm, WIFI_POWER_21dBm, WIFI_POWER_20_5dBm, WIFI_POWER_20dBm,
   WIFI_POWER_19_5dBm, WIFI_POWER_19dBm, WIFI_POWER_18_5dBm, WIFI_POWER_17dBm,
   WIFI_POWER_15dBm,   WIFI_POWER_13dBm, WIFI_POWER_11dBm,   WIFI_POWER_8_5dBm,
   WIFI_POWER_7dBm,    WIFI_POWER_5dBm,  WIFI_POWER_2dBm,    WIFI_POWER_MINUS_1dBm};
 
 const char *powerstr[powerCount] = {
-  "WIFI_POWER_default",
+  "WIFI_POWER_default", "WIFI_POWER_21dBm", "WIFI_POWER_20_5dBm", "WIFI_POWER_20dBm",
   "WIFI_POWER_19_5dBm", "WIFI_POWER_19dBm", "WIFI_POWER_18_5dBm", "WIFI_POWER_17dBm",
   "WIFI_POWER_15dBm",   "WIFI_POWER_13dBm", "WIFI_POWER_11dBm",   "WIFI_POWER_8_5dBm",
   "WIFI_POWER_7dBm",    "WIFI_POWER_5dBm",  "WIFI_POWER_2dBm",    "WIFI_POWER_MINUS_1dBm"};
 
-int truepower[powerCount] = {0};
-
+  // TX power can be set to the corresponding wifi_power_t
+bool validpower[powerCount] = {false};
 long ctimes[powerCount] = {0};
-
+long iptimes[powerCount] = {0};
 int rssi[powerCount] = {0};
 
 void print_ctimes(void) {
-  bool err = false;
-  Serial.println("Connect time vs txPower\n");
+  int invalidpowers = 0;
+  int noconnects = 0;
+  Serial.println("\n\nConnect time vs txPower\n");
   Serial.printf("\nBoard: %s\n", STA_MAC_STR);
-
-  Serial.printf("%24s | %5s | %5s | %5s | %8s\n", "enum", "want", "set", "rssi", "time (ms)");
-  Serial.printf("%26s %7s %7s %7s\n", "|", "|", "|", "|");
+  Serial.printf("%24s | %3s | %4s | %4s | %6s | %6s \n", "enum", "set", "dBm", "rssi", "time", "dhcp");
+  Serial.printf("%26s %5s %6s %6s %8s\n", "|", "|", "|", "|", "|");
   for (int i=0; i<powerCount; i++) {
-     Serial.printf("%24s | %5d | %5d | %5d | %8lu", powerstr[i], power[i], truepower[i], rssi[i], ctimes[i]);
-     if (truepower[i] != power[i] ) {
-      Serial.print(" *");
-      err = true;
+     Serial.printf("%24s | %3d | %4.1f |", powerstr[i], power[i], 0.25*power[i]);
+     if (validpower[i]) {
+       if (ctimes[i] < TIMEOUT) {
+         Serial.printf(" %4d | %6lu | %6lu", rssi[i], ctimes[i], iptimes[i]);
+       } else {
+        noconnects++;
+        Serial.printf(" %4s | %6s | %6s", "-", "-", "-");
+       }  
+     } else {
+       invalidpowers++;
+       Serial.printf(" %4s | %6s | %6s", "*", "*", "*");
      }
      Serial.println();
   }
-  if (err)
-    Serial.println("  * = unable to set transmit power to desired value");
+  if (invalidpowers) {
+    Serial.println("  (*) Invalid TX power value");
+  }
+  if (noconnects) {
+    Serial.printf("  (-) Could not connect in %.1f seconds\n", TIMEOUT/1000.0);
+  }
 }
+
 
 unsigned long connect_time = 0;
 unsigned long etime = 0;
@@ -108,7 +123,6 @@ unsigned long dottime = 0;
 void start_connecting(int pwindex = 0) {
   if ((pwindex < 0) || (pwindex >= powerCount)) return;
   
-  Serial.printf("\nTest #%d/%d\n", pwindex, powerCount-1);
   if (WiFi.isConnected()) {
     Serial.print("Disconnecting...");
     WiFi.setAutoReconnect(false);
@@ -123,11 +137,7 @@ void start_connecting(int pwindex = 0) {
   int cpower = power[pwindex];
   WiFi.setTxPower((wifi_power_t)cpower); // can return false, will pick up "error"
   delay(25);
-  int tpower = WiFi.getTxPower();
-  truepower[pwindex] = tpower;
-  Serial.printf("Power level index: %d. Wanted txPower %s (%d), set to %d = %.1f dBm\n", pwindex, powerstr[pwindex], cpower, tpower, tpower*0.25);
-  Serial.print("Connecting to Wi-Fi network... ");
- 
+  Serial.print("Connecting to Wi-Fi network ");
   connect_time = millis();
   dottime = millis();
   WiFi.begin(ssid, password);
@@ -135,33 +145,87 @@ void start_connecting(int pwindex = 0) {
 
 void getStatus(void) {
   switch(WiFi.status()) {
+      case WL_IDLE_STATUS:
+        Serial.println("[WiFi] Idle");
+        break;
       case WL_NO_SSID_AVAIL:
         Serial.println("[WiFi] SSID not found");
-        break;
-      case WL_CONNECT_FAILED:
-        Serial.print("[WiFi] Failed - WiFi not connected! Reason: ");
-        return;
-        break;
-      case WL_CONNECTION_LOST:
-        Serial.println("[WiFi] Connection was lost");
         break;
       case WL_SCAN_COMPLETED:
         Serial.println("[WiFi] Scan is completed");
         break;
-      case WL_DISCONNECTED:
-        Serial.println("[WiFi] WiFi is disconnected");
-        break;
       case WL_CONNECTED:
-        Serial.print("[WiFi] WiFi is connected. IP address: ");
-        Serial.println(WiFi.localIP());
+        Serial.println("[WiFi] Connected");
+        break;  
+      case WL_CONNECT_FAILED:
+        Serial.print("[WiFi] Connection Failed");
         break;
+      case WL_CONNECTION_LOST:
+        Serial.println("[WiFi] Connection was lost");
+        break;
+      case WL_DISCONNECTED:
+        Serial.println("[WiFi] Disconnected");
+        break;
+      case WL_NO_SHIELD:
+        Serial.println("[WiFi] No shield"); // should never happen
+        break; 
       default:
-        Serial.print("[WiFi] WiFi Status: ");
+        Serial.print("[WiFi] Unknown status: ");
         Serial.println(WiFi.status());
         break;
   }
 }
 
+int txIndex = -1;
+
+void run_test(void) {
+  txIndex++;
+  if (txIndex >= powerCount) return;
+
+  Serial.printf("\nTest with %s\n", powerstr[txIndex]);
+
+  if (!validpower[txIndex]) {
+    Serial.println("This power level cannot be set");
+    return;
+  }
+
+  start_connecting(txIndex);
+  unsigned long cctime = 0;
+
+  while (true) {
+    if (millis() - dottime > 2000) {
+      Serial.print('.');
+      dottime = millis();
+    }
+
+    if (WiFi.STA.connected()) {
+      if (!cctime) { 
+        cctime = millis() - connect_time;
+      }  
+    }  
+    
+    if (WiFi.isConnected()) {
+      etime = millis() - connect_time;
+      ctimes[txIndex] = cctime;
+      iptimes[txIndex] = etime;
+      Serial.print("\nConnected with IP address: ");
+      Serial.println(WiFi.localIP());
+      rssi[txIndex] = WiFi.RSSI();
+      Serial.printf("RSSI: %d dBm\n", rssi[txIndex]);   
+      Serial.printf("Time to connect: %lu ms\n", cctime);
+      Serial.printf("Time to IP address acquisition: %lu ms\n", etime);
+      return;
+    }
+
+    if (millis() - connect_time > TIMEOUT) {
+      Serial.printf("\nNot connected after %d ms\n", TIMEOUT);
+      getStatus();
+      ctimes[txIndex] = 2*TIMEOUT;
+      return;
+    }
+    yield(); // keep the watchdog happy
+  }
+}  
 
 void setup() {
   #if !defined(SERIAL_BEGIN_DELAY)
@@ -205,58 +269,24 @@ void setup() {
   WiFi.mode(WIFI_STA);
   delay(25);
   power[0] = WiFi.getTxPower();  // default tx power on boot
-  start_connecting(0);
-}
+  Serial.printf("\nThe default TX power level is %d = %.1f dBm\n", power[0], power[0]*0.25);
 
-int txIndex = 0;
-unsigned long cctime = 0;
+  Serial.println("Testing setting TX power levels");
+  for (int i=0; i < powerCount; i++) {
+    int pwr = power[i];
+    WiFi.setTxPower((wifi_power_t)pwr); // can return false, will pick up "error"
+    delay(25);
+    int res = WiFi.getTxPower();
+    validpower[i] = (res == pwr);
+    Serial.printf("Setting power level %18s %s\n", powerstr[i], (validpower[i]) ? "succeeds"  : "fails");
+  }  
+
+  while (txIndex < powerCount) {
+    run_test();
+  }  
+  print_ctimes();
+}
 
 void loop() {
-  if (txIndex == powerCount)  {
-    print_ctimes();
-    txIndex++;
-  }
-  if (txIndex > powerCount) return;
-
-  if (millis() - dottime > 2000) {
-    Serial.print('.');
-    dottime = millis();
-  }
-
-  if (WiFi.STA.connected()) {
-    if (!cctime) { 
-      cctime = millis() - connect_time;
-    }  
-  }  
-  if (WiFi.isConnected()) {
-    etime = millis() - connect_time;
-    delay(1500); // see wifi_blackhole
-    Serial.print(" connected with IP address: ");
-    Serial.println(WiFi.localIP());
-    Serial.printf("Time to connect: %lu ms\n", cctime);
-    Serial.printf("Time to IP address acquisition: %lu ms\n", etime);
-    ctimes[txIndex] = cctime;
-    rssi[txIndex] = WiFi.RSSI();
-    Serial.printf("RSSI: %d dBm\n", rssi[txIndex]);   
-    int txpower = WiFi.getTxPower(); // just checking
-    Serial.printf("Tx Power: %d  %.1f dBm\n", txpower, txpower*0.25);  
-
-    delay(1000);
-    txIndex++;
-    if (txIndex >= powerCount) return;
-    cctime = 0;
-    start_connecting(txIndex);
-  }
-
-  if (millis() - connect_time > TIMEOUT) {
-    Serial.printf("\nNot connected after %d ms\n", TIMEOUT);
-    getStatus();
-    ctimes[txIndex] = 2*TIMEOUT;
-    txIndex++;
-    if (txIndex >= powerCount) return;
-    Serial.println("Retrying in 5 seconds with different txPower");
-    delay(5000);
-    cctime = 0;
-    start_connecting(txIndex);
-  }
-}
+  // do nothing
+}  
